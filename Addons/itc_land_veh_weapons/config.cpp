1da04c70c7e76cd549aaa2f3b25b6952c43cdbd8
Andrew Somerville <asmsandrew@googlemail.com>
2018-04-14 03:13:22 +0100

    Initial Config Work

---
+++ Addons/itc_land_veh_weapons/config.cpp
@@ -0,0 +1,16 @@
+class CfgPatches {
+	class itc_land_veh_weapons {
+		author = "ITC Addons Team";
+		authors[] = {"ToadBall","Yax","VKing"};
+		requiredaddons[] = {"A3_Weapons_F"};
+		requiredversion = 1.8;
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+	};
+};
+
+#include "ITC_Land_CfgFuzes.hpp"
+#include "CfgFunctions.hpp"
+#include "CfgWeapons.hpp"
+#include "CfgMagazines.hpp"
