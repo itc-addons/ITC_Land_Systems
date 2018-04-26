6b0d66039b92f2829145bb8c40832aceb4465e5f
Andrew Somerville <asmsandrew@googlemail.com>
2018-04-26 22:55:52 +0100

    Ammo Handling Flags

--- Addons/itc_land_veh_weapons/CfgMagazines_152mm.hpp
+++ Addons/itc_land_veh_weapons/CfgMagazines_152mm.hpp
@@ -6,13 +6,14 @@
 		displayNameShort = "G152 HE";
 		itc_land_fuze = "mof35_mod0";
 		itc_land_submunition = "itc_land_155mm_helper";	
-		itc_land_charge_format = "itc_land_g152hex_chg%1";		
+		itc_land_charge_format = "itc_land_g152hex_chg%1";	
+		itc_land_CountInAH = 1;				
 	};
-	class itc_land_g152hex_chg1: itc_land_g152hex { initSpeed = 165.6; };	
-	class itc_land_g152hex_chg2: itc_land_g152hex { initSpeed = 331.2; };
-	class itc_land_g152hex_chg3: itc_land_g152hex { initSpeed = 496.8; };
-	class itc_land_g152hex_chg4: itc_land_g152hex { initSpeed = 662.4; };
-	class itc_land_g152hex_chg5: itc_land_g152hex { initSpeed = 828; };
+	class itc_land_g152hex_chg1: itc_land_g152hex { initSpeed = 165.6; itc_land_CountInAH = 0;   };	
+	class itc_land_g152hex_chg2: itc_land_g152hex { initSpeed = 331.2; itc_land_CountInAH = 0;  };
+	class itc_land_g152hex_chg3: itc_land_g152hex { initSpeed = 496.8; itc_land_CountInAH = 0;  };
+	class itc_land_g152hex_chg4: itc_land_g152hex { initSpeed = 662.4; itc_land_CountInAH = 0;  };
+	class itc_land_g152hex_chg5: itc_land_g152hex { initSpeed = 828; itc_land_CountInAH = 0;  };
 	
 	class itc_land_g152smo: itc_land_how_mag {
 		author = "Toadball";
@@ -21,13 +22,14 @@
 		muzzleImpulseFactor[] = {15, 30};
 		displayNameShort = "G152 SMOKE";
 		itc_land_fuze = "mof35_mod1";
-		itc_land_charge_format = "itc_land_g152smo_chg%1";		
+		itc_land_charge_format = "itc_land_g152smo_chg%1";
+		itc_land_CountInAH = 1;				
 	};
-	class itc_land_g152smo_chg1: itc_land_g152smo { initSpeed = 165.6; };
-	class itc_land_g152smo_chg2: itc_land_g152smo { initSpeed = 331.2; };	
-	class itc_land_g152smo_chg3: itc_land_g152smo { initSpeed = 496.8; };	
-	class itc_land_g152smo_chg4: itc_land_g152smo { initSpeed = 662.4; };
-	class itc_land_g152smo_chg5: itc_land_g152smo { initSpeed = 828; };
+	class itc_land_g152smo_chg1: itc_land_g152smo { initSpeed = 165.6; itc_land_CountInAH = 0;   };
+	class itc_land_g152smo_chg2: itc_land_g152smo { initSpeed = 331.2; itc_land_CountInAH = 0;   };	
+	class itc_land_g152smo_chg3: itc_land_g152smo { initSpeed = 496.8; itc_land_CountInAH = 0;   };	
+	class itc_land_g152smo_chg4: itc_land_g152smo { initSpeed = 662.4; itc_land_CountInAH = 0;   };
+	class itc_land_g152smo_chg5: itc_land_g152smo { initSpeed = 828; itc_land_CountInAH = 0;   };
 	
 	class itc_land_g152icm: itc_land_how_mag {
 		author = "Toadball";
@@ -36,13 +38,14 @@
 		muzzleImpulseFactor[] = {15, 30};
 		displayNameShort = "G152 ICM";
 		itc_land_fuze = "mof35_mod1";
-		itc_land_charge_format = "itc_land_g152icm_chg%1";				
+		itc_land_charge_format = "itc_land_g152icm_chg%1";	
+		itc_land_CountInAH = 1;				
 	}; 
-	class itc_land_g152icm_chg1: itc_land_g152icm { initSpeed = 165.6; };	
-	class itc_land_g152icm_chg2: itc_land_g152icm { initSpeed = 331.2; };	
-	class itc_land_g152icm_chg3: itc_land_g152icm { initSpeed = 496.8; };	
-	class itc_land_g152icm_chg4: itc_land_g152icm { initSpeed = 662.4; };
-	class itc_land_g152icm_chg5: itc_land_g152icm { initSpeed = 828; };
+	class itc_land_g152icm_chg1: itc_land_g152icm { initSpeed = 165.6; itc_land_CountInAH = 0;   };	
+	class itc_land_g152icm_chg2: itc_land_g152icm { initSpeed = 331.2; itc_land_CountInAH = 0;   };	
+	class itc_land_g152icm_chg3: itc_land_g152icm { initSpeed = 496.8; itc_land_CountInAH = 0;   };	
+	class itc_land_g152icm_chg4: itc_land_g152icm { initSpeed = 662.4; itc_land_CountInAH = 0;   };
+	class itc_land_g152icm_chg5: itc_land_g152icm { initSpeed = 828; itc_land_CountInAH = 0;   };
 
 	class itc_land_g152mat: itc_land_how_mag {
 		author = "Toadball";
@@ -51,13 +54,14 @@
 		muzzleImpulseFactor[] = {15, 30};
 		displayNameShort = "G152 MAT";
 		itc_land_fuze = "mof35_mod1";
-		itc_land_charge_format = "itc_land_g152mat_chg%1";				
+		itc_land_charge_format = "itc_land_g152mat_chg%1";	
+		itc_land_CountInAH = 1;				
 	}; 
-	class itc_land_g152mat_chg1: itc_land_g152mat { initSpeed = 165.6; };	
-	class itc_land_g152mat_chg2: itc_land_g152mat { initSpeed = 331.2; };	
-	class itc_land_g152mat_chg3: itc_land_g152mat { initSpeed = 496.8; };	
-	class itc_land_g152mat_chg4: itc_land_g152mat { initSpeed = 662.4; };
-	class itc_land_g152mat_chg5: itc_land_g152mat { initSpeed = 828; };	
+	class itc_land_g152mat_chg1: itc_land_g152mat { initSpeed = 165.6; itc_land_CountInAH = 0;   };	
+	class itc_land_g152mat_chg2: itc_land_g152mat { initSpeed = 331.2; itc_land_CountInAH = 0;   };	
+	class itc_land_g152mat_chg3: itc_land_g152mat { initSpeed = 496.8; itc_land_CountInAH = 0;   };	
+	class itc_land_g152mat_chg4: itc_land_g152mat { initSpeed = 662.4; itc_land_CountInAH = 0;   };
+	class itc_land_g152mat_chg5: itc_land_g152mat { initSpeed = 828; itc_land_CountInAH = 0;   };	
 
 	class itc_land_g152map: itc_land_how_mag {
 		author = "Toadball";
@@ -66,13 +70,14 @@
 		muzzleImpulseFactor[] = {15, 30};
 		displayNameShort = "G152 MAP";
 		itc_land_fuze = "mof35_mod1";
-		itc_land_charge_format = "itc_land_g152map_chg%1";				
+		itc_land_charge_format = "itc_land_g152map_chg%1";	
+		itc_land_CountInAH = 1;				
 	}; 
-	class itc_land_g152map_chg1: itc_land_g152map { initSpeed = 165.6; };	
-	class itc_land_g152map_chg2: itc_land_g152map { initSpeed = 331.2; };	
-	class itc_land_g152map_chg3: itc_land_g152map { initSpeed = 496.8; };	
-	class itc_land_g152map_chg4: itc_land_g152map { initSpeed = 662.4; };
-	class itc_land_g152map_chg5: itc_land_g152map { initSpeed = 828; };
+	class itc_land_g152map_chg1: itc_land_g152map { initSpeed = 165.6; itc_land_CountInAH = 0;   };	
+	class itc_land_g152map_chg2: itc_land_g152map { initSpeed = 331.2; itc_land_CountInAH = 0;   };	
+	class itc_land_g152map_chg3: itc_land_g152map { initSpeed = 496.8; itc_land_CountInAH = 0;   };	
+	class itc_land_g152map_chg4: itc_land_g152map { initSpeed = 662.4; itc_land_CountInAH = 0;   };
+	class itc_land_g152map_chg5: itc_land_g152map { initSpeed = 828; itc_land_CountInAH = 0;   };
 
 	class itc_land_g152pgm: itc_land_how_mag {
 		author = "Toadball";
@@ -83,14 +88,15 @@
 		itc_land_fuze = "mof35_mod2";
 		itc_land_guidance = "gps_inertial";
 		itc_land_submunition = "itc_land_155mm_helper";	
-		itc_land_charge_format = "itc_land_g152pgm_chg%1";						
+		itc_land_charge_format = "itc_land_g152pgm_chg%1";
+		itc_land_CountInAH = 1;				
 
 	}; 
-	class itc_land_g152pgm_chg1: itc_land_g152pgm { initSpeed = 165.6; };	
-	class itc_land_g152pgm_chg2: itc_land_g152pgm { initSpeed = 331.2; };	
-	class itc_land_g152pgm_chg3: itc_land_g152pgm { initSpeed = 496.8; };	
-	class itc_land_g152pgm_chg4: itc_land_g152pgm { initSpeed = 662.4; };
-	class itc_land_g152pgm_chg5: itc_land_g152pgm { initSpeed = 828; };
+	class itc_land_g152pgm_chg1: itc_land_g152pgm { initSpeed = 165.6; itc_land_CountInAH = 0;   };	
+	class itc_land_g152pgm_chg2: itc_land_g152pgm { initSpeed = 331.2; itc_land_CountInAH = 0;  };	
+	class itc_land_g152pgm_chg3: itc_land_g152pgm { initSpeed = 496.8; itc_land_CountInAH = 0;  };	
+	class itc_land_g152pgm_chg4: itc_land_g152pgm { initSpeed = 662.4; itc_land_CountInAH = 0;  };
+	class itc_land_g152pgm_chg5: itc_land_g152pgm { initSpeed = 828; itc_land_CountInAH = 0;  };
 
 	class itc_land_g152lgm: itc_land_how_mag {
 		author = "Toadball";
@@ -102,12 +108,13 @@
 		itc_land_guidance = "laser_coded";
 		itc_land_submunition = "itc_land_155mm_helper";	
 		itc_land_charge_format = "itc_land_g152lgm_chg%1";
+		itc_land_CountInAH = 1;				
 	}; 
-	class itc_land_g152lgm_chg1: itc_land_g152lgm { initSpeed = 165.6; };	
-	class itc_land_g152lgm_chg2: itc_land_g152lgm { initSpeed = 331.2; };	
-	class itc_land_g152lgm_chg3: itc_land_g152lgm { initSpeed = 496.8; };	
-	class itc_land_g152lgm_chg4: itc_land_g152lgm { initSpeed = 662.4; };
-	class itc_land_g152lgm_chg5: itc_land_g152lgm { initSpeed = 828; };	
+	class itc_land_g152lgm_chg1: itc_land_g152lgm { initSpeed = 165.6; itc_land_CountInAH = 0;  };	
+	class itc_land_g152lgm_chg2: itc_land_g152lgm { initSpeed = 331.2; itc_land_CountInAH = 0;  };	
+	class itc_land_g152lgm_chg3: itc_land_g152lgm { initSpeed = 496.8; itc_land_CountInAH = 0;  };	
+	class itc_land_g152lgm_chg4: itc_land_g152lgm { initSpeed = 662.4; itc_land_CountInAH = 0;  };
+	class itc_land_g152lgm_chg5: itc_land_g152lgm { initSpeed = 828; itc_land_CountInAH = 0;  };	
 
 	class itc_land_g152ill: itc_land_how_mag {
 		author = "Toadball";
@@ -117,9 +124,10 @@
 		displayNameShort = "G152 ILLUM";
 		itc_land_fuze = "mof35_mod1";
 		itc_land_charge_format = "itc_land_g152ill_chg%1";
+		itc_land_CountInAH = 1;				
 	}; 
-	class itc_land_g152ill_chg1: itc_land_g152ill { initSpeed = 165.6; };	
-	class itc_land_g152ill_chg2: itc_land_g152ill { initSpeed = 331.2; };	
-	class itc_land_g152ill_chg3: itc_land_g152ill { initSpeed = 496.8; };	
-	class itc_land_g152ill_chg4: itc_land_g152ill { initSpeed = 662.4; };
-	class itc_land_g152ill_chg5: itc_land_g152ill { initSpeed = 828; };	
+	class itc_land_g152ill_chg1: itc_land_g152ill { initSpeed = 165.6; itc_land_CountInAH = 0;  };	
+	class itc_land_g152ill_chg2: itc_land_g152ill { initSpeed = 331.2; itc_land_CountInAH = 0;  };	
+	class itc_land_g152ill_chg3: itc_land_g152ill { initSpeed = 496.8; itc_land_CountInAH = 0;  };	
+	class itc_land_g152ill_chg4: itc_land_g152ill { initSpeed = 662.4; itc_land_CountInAH = 0;  };
+	class itc_land_g152ill_chg5: itc_land_g152ill { initSpeed = 828; itc_land_CountInAH = 0;  };	
