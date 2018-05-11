35b0de1711674c3239092e64ab77ed6551c02282
Andrew Somerville <asmsandrew@googlemail.com>
2018-05-12 00:58:28 +0100

    Illum Fix

--- Addons/itc_land_veh_weapons/CfgAmmo.hpp
+++ Addons/itc_land_veh_weapons/CfgAmmo.hpp
@@ -6,6 +6,7 @@ class CfgAmmo {
 		aimAboveTarget[] = {50, 80, 140, 200, 260, 320, 380};
 		aimAboveDefault = 4;
 		triggerSpeedCoef = 1;
+		triggerTime = 0.001;
 		lightColor[] = {0.95, 0.95, 1, 0.5};
 		smokeColor[] = {1, 1, 1, 0.5};
 		brightness = 200;
@@ -13,7 +14,7 @@ class CfgAmmo {
 		//flareSize = 500;
 		timeToLive = 90;
 		ace_rearm_caliber = 155;
-		explosionTime = 0.0001;
+		//explosionTime = 1;
 	};
 	class Sh_155mm_AMOS;
 	class itc_land_155mm: Sh_155mm_AMOS {
