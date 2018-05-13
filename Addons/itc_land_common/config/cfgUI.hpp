ae3cdf48332df1bb87fa15a1d0f9d40e6e6348d9
Andrew Somerville <asmsandrew@googlemail.com>
2018-05-13 13:24:26 +0100

    Migration of UI Defines to Common

---
+++ Addons/itc_land_common/config/cfgUI.hpp
@@ -0,0 +1,103 @@
+class ITC_ARTY_RscText;
+class ITC_LAND_RscText: ITC_ARTY_RscText {};
+class ITC_ARTY_RscTextBox;
+class ITC_LAND_RscTextBox: ITC_ARTY_RscTextBox {};
+class ITC_ARTY_RscListBox;
+class ITC_LAND_RscListBox: ITC_ARTY_RscListBox {};
+class ITC_ARTY_RscButton;
+class ITC_LAND_RscButton: ITC_ARTY_RscButton {};
+class ITC_ARTY_RscComboBox;
+class ITC_LAND_RscComboBox: ITC_ARTY_RscComboBox {};
+
+class RscFrame;	
+class ITC_LAND_RscFrame: RscFrame {
+	style = 0xA0;
+	colorBackground[] = {0,0,0,0};
+	colorText[] = AIFMSGRN;		
+};	
+class RscText;
+class ITC_LAND_RscText: RscText {
+	colorText[] = AIFMSGRN;
+};
+class RscEdit;
+class ITC_LAND_RscEdit: RscEdit {
+	colorText[] = AIFMSGRN;
+	colorSelection[] = {0.1,0.1,0.1,0.85};
+};
+class RscStructuredText;
+class ITC_LAND_RscStructuredText: RscStructuredText {
+	colorText[] = AIFMSGRN;
+    class Attributes {
+        font = "RobotoCondensed";
+        color = "#acff99";
+        colorLink = "#acff99";
+        align = "left";
+        shadow = 1;
+    };
+};
+class RscPicture;
+class ITC_LAND_RscPicture: RscPicture {
+	colorText[] = AIFMSGRN;
+	tooltipColorText[] = AIFMSGRN;
+	tooltipColorBox[] = AIFMSGRN;
+};
+class RscControlsGroup;
+class ITC_LAND_Workspace: RscControlsGroup {
+	onLoad = "(_this # 0) ctrlShow false";
+	x = (0.344062+ POSXADJUST) * safezoneW + safezoneX;
+	y = 0.225 * safezoneH + safezoneY;
+	w = 0.622812 * safezoneW;
+	h = 0.506 * safezoneH;
+};
+class ScrollBar;
+class ITC_LAND_ScrollBar: ScrollBar {
+		color[] = AIFMSGRN;
+		colorActive[] = AIFMSGRN;
+};
+
+class RscListBox;
+class ITC_LAND_RscListBox: RscListBox {
+	colorText[] = AIFMSGRN;
+	colorScrollbar[] = AIFMSGRN;
+	colorSelect[] = {0, 0, 0, 1};
+	colorSelect2[] = {0, 0, 0, 1};
+	colorSelectBackground[] = AIFMSGRN;
+	colorSelectBackground2[] = AIFMSGRN;
+	arrowEmpty = AIFMSGRN;
+	arrowFull = AIFMSGRN;
+
+	tooltipColorText[] = AIFMSGRN;
+	tooltipColorBox[] = AIFMSGRN;
+	tooltipColorShade[] = {0, 0, 0, 0.65};
+	class ListScrollBar: ITC_LAND_ScrollBar	{
+		color[] = AIFMSGRN;
+		autoScrollEnabled = 1;
+	};
+};
+class RscCombo;
+class ITC_LAND_RscComboBox: RscCombo {
+	colorText[] = AIFMSGRN;
+	colorSelectBackground[] = AIFMSGRN;
+	colorScrollbar[] = AIFMSGRN;
+	colorPictureSelected[] = AIFMSGRN;
+	colorTextRight[] = AIFMSGRN;
+	colorSelectRight[] = {0,0,0,1};
+	colorSelect2Right[] = {0,0,0,1};
+	tooltipColorText[] = AIFMSGRN;
+	tooltipColorBox[] = AIFMSGRN;
+	arrowEmpty = "itc_land_common\data\arrow_combo_ca.paa";
+	arrowFull = "itc_land_common\data\arrow_combo_active_ca.paa";
+	colorActive[] = AIFMSGRN;
+};
+
+class RscButton;
+class ITC_LAND_RscButton {
+	colorText[] = AIFMSGRN;
+	colorDisabled[] = {1,1,1,0.25};
+	colorBackground[] = {0,0,0,0.5};
+	colorBackgroundDisabled[] = {0,0,0,0.5};
+	colorBackgroundActive[] = {0.2,0.2,0.2,1};
+	colorBorder[] = AIFMSGRN;
+};
+
+
