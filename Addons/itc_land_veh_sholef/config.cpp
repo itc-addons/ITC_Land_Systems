23792b0bcc49577ae4efe498bed1a79713cedc28
Andrew Somerville <asmsandrew@googlemail.com>
2018-05-13 23:49:37 +0100

    CfgPatches - Sholef Req Version

--- Addons/itc_land_veh_sholef/config.cpp
+++ Addons/itc_land_veh_sholef/config.cpp
@@ -2,7 +2,7 @@
 	class itc_land_veh_sholef	{
 		units[] = {	"itc_land_b_SPH_Sholef2","itc_land_b_t_SPH_Sholef2" };
 		weapons[] = {};
-		requiredVersion = 0.10;
+		requiredVersion = 1.8;
 		requiredAddons[] = {"A3_Weapons_F","A3_Armor_F_Gamma_MBT_01","itc_land_veh_sights","itc_land_veh_weapons","itc_land_sphammohandler"};
 		author = "ITC Addons Team";
 		authors[] = {"ToadBall","Yax"};
