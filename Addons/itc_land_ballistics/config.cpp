fcbbbc1fa02227c0c7f6cf3394ac7c04165fdb82
Andrew Somerville <asmsandrew@googlemail.com>
2018-05-14 00:12:27 +0100

    Updating CfgPatches - Balistics, BCS, Tablet

--- Addons/itc_land_ballistics/config.cpp
+++ Addons/itc_land_ballistics/config.cpp
@@ -2,7 +2,7 @@ class CfgPatches {
 	class itc_land_ballistics {
 		author = "ITC Addons Team";
 		authors[] = {"ToadBall","Yax","VKing"};
-		requiredaddons[] = {"A3_Weapons_F"};
+		requiredaddons[] = {"A3_Weapons_F","ace_common","ace_mk6mortar"};
 		requiredversion = 1.8;
 		units[] = {};
 		weapons[] = {};
--- Addons/itc_land_tablet/config.cpp
+++ Addons/itc_land_tablet/config.cpp
@@ -2,15 +2,25 @@ class CfgPatches {
 	class itc_land_tablet {
 		author = "ITC Addons Team";
 		authors[] = {"ToadBall","Yax","VKing"};
-		requiredaddons[] = {"A3_Weapons_F"};
+		requiredaddons[] = {"A3_Weapons_F","itc_land_common","itc_land_bcs","ace_interaction","ace_interact_menu","ace_common"};
 		requiredversion = 1.8;
 		units[] = {};
-		weapons[] = {};
+		weapons[] = {"itc_land_tablet_spg"};
 		magazines[] = {};
 	};
 };
 
-#include "UIconfig\ControlBaseClasses.hpp"
+class ITC_LAND_RscFrame;
+class ITC_LAND_RscText;
+class ITC_LAND_RscEdit;
+class ITC_LAND_RscStructuredText;
+class ITC_LAND_RscPicture;
+class ITC_LAND_Workspace;
+class ITC_LAND_ScrollBar;
+class ITC_LAND_RscListBox;
+class ITC_LAND_RscComboBox;
+class ITC_LAND_RscButton;
+
 #include "UIconfig\tablet.hpp"
 
 #include "config\cfgVehicles.hpp"
