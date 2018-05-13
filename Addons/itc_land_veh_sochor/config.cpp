795ea6fb5a5cbe8b0123bc12da3cf409ed65ce29
Andrew Somerville <asmsandrew@googlemail.com>
2018-05-13 23:48:45 +0100

    Updating CfgPatches - Seara, Sholef, Sochor

--- Addons/itc_land_veh_sochor/config.cpp
+++ Addons/itc_land_veh_sochor/config.cpp
@@ -1,8 +1,8 @@
  class CfgPatches {
 	class itc_land_veh_sochor	{
-		units[] = {	"itc_land_sph_sochor2"};
+		units[] = {	"itc_land_o_sph_sochor2","itc_land_o_t_sph_sochor2"};
 		weapons[] = {};
-		requiredVersion = 0.10;
+		requiredVersion = 1.8;
 		requiredAddons[] = {"A3_Weapons_F","itc_land_veh_sights","itc_land_veh_weapons"};
 		author = "ITC Addons Team";
 		authors[] = {"ToadBall","Yax"};
