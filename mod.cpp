b1518b8a0bbb79b3c8a7c4a5276da5f8a34bfc60
Yannik Hegge <yannikhegge@gmail.com>
2018-05-17 20:37:15 +0200

    mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,13 +1,14 @@
+/// Name of your mod
 name = "ITC Land Systems";
-picture = " ";
-actionName = "";
-action = "";
-logo = "logo_ALFR_small_ca.paa";
-logoOver = " ";
-tooltip = " ";
-tooltipOwned = " ";
-overview = " ";
-author = "ITC Addons Team";
-overviewPicture = "logo_ALFR_ca.paa";
-overviewText = "ITC Addons - Land Systems";
-overviewFootnote = "";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "itc-combo.paa";
+/// Display next to the item added by the mod
+logoSmall = "itc-combo.paa";
+/// Logo displayed in the main menu
+logo = "itc-combo.paa";
+/// When the mouse is over, in the main menu
+logoOver = "itc-combo.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "https://github.com/itc-addons/ITC_Land_Systems";
+/// Overview text, displayed from the extension menu
+overview = "ITC Land Systems.";
