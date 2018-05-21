e3989b1d263e4d26e3f54327faa2addcd97f0934
Andrew Somerville <asmsandrew@googlemail.com>
2018-05-21 13:03:17 +0100

    Model & Data

---
+++ Addons/itc_land_ammohandling/data/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+	class ace_sys_arty_ammunition {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.62;
+		requiredAddons[] = {"CAWeapons","ace_sys_arty","ace_main","ace_sys_interaction","ace_sys_huntir","ace_sys_disposal","ace_sys_crewserved","ace_sys_arty_ammunition"};
+		author[] = {"ToadBall"};
+	};
+};
+
+class CfgAmmo {
+	
+};	
