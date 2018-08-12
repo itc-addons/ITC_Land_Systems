8f40d0dd76c76ef5c80a1b9da39e61db224718e4
Andrew Somerville <asmsandrew@googlemail.com>
2018-08-12 22:26:39 +0100

    Housekeeping - Rhea 2
    Moved from itc_land_s750 to itc_land_rhea.
    Updated classnames to match with ITC Land convention

---
+++ Addons/itc_land_veh_rhea/config.cpp
@@ -0,0 +1,12 @@
+ class CfgPatches {
+	class itc_land_veh_rhea	{
+		units[] = {	"itc_land_o_sam_rhea2"};
+		weapons[] = {};
+		requiredVersion = 1.8;
+		requiredAddons[] = {"A3_Weapons_F_Sams","itc_land_veh_weapons"};
+		author = "ITC Addons Team";
+		authors[] = {"ToadBall","Yax","VKing"};
+	};
+};
+
+#include "CfgVehicles.hpp"
