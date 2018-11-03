7b9192f7c8462ab83d5e0b3ece539c8d9881efed
Yannik Hegge <yannikhegge@gmail.com>
2018-11-03 14:45:17 +0100

    Mk6 rangetables & illumination improvement

---
+++ Addons/itc_land_veh_mk6/config.cpp
@@ -0,0 +1,14 @@
+class CfgPatches {
+	class itc_land_veh_mk6 {
+		author = "ITC Addons Team";
+		authors[] = {"ToadBall","Yax","VKing"};
+		requiredaddons[] = {"A3_Weapons_F"};
+		requiredversion = 1.8;
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+	};
+};
+
+#include "config\cfgMagazines.hpp"
+#include "config\cfgAmmo.hpp"
