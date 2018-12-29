068efac02fa86216d752ede5bcc17c195c3f01a3
Andrew Somerville <asmsandrew@googlemail.com>
2018-12-29 13:39:43 +0000

    Revert "oops.cpp"
    This reverts commit 1bb25f84a53768e3d642f45496d1a1d7b775c97f.

--- Addons/itc_land_datalink/config.cpp
+++ Addons/itc_land_datalink/config.cpp
@@ -1,8 +1,8 @@
 class CfgPatches {
-	class itc_land_datalink {
+	class itc_land_ballistics {
 		author = "ITC Addons Team";
 		authors[] = {"ToadBall","Yax","VKing"};
-		requiredaddons[] = {"cba_common"};
+		requiredaddons[] = {"A3_Weapons_F","ace_common","ace_mk6mortar"};
 		requiredversion = 1.8;
 		units[] = {};
 		weapons[] = {};
