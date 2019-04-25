9755478d9a682befe3378e33829d3b1dbb9e32b1
Andrew Somerville <asmsandrew@googlemail.com>
2019-04-25 20:21:36 +0100

    NGS Vehicle creation

---
+++ Addons/itc_land_veh_hammer/CfgVehicles.hpp
@@ -0,0 +1,75 @@
+/// Magazines macros definition ///
+
+#define mag_2(a) a, a
+#define mag_3(a) a, a, a
+#define mag_4(a) a, a, a, a
+#define mag_5(a) a, a, a, a, a
+#define mag_6(a) a, a, a, a, a, a
+#define mag_7(a) a, a, a, a, a, a, a
+#define mag_8(a) a, a, a, a, a, a, a, a
+#define mag_9(a) a, a, a, a, a, a, a, a, a
+#define mag_10(a) a, a, a, a, a, a, a, a, a, a
+#define mag_11(a) a, a, a, a, a, a, a, a, a, a, a
+#define mag_12(a) a, a, a, a, a, a, a, a, a, a, a, a
+#define mag_24(a) a, a, a, a, a, a, a, a, a, a, a, a,a, a, a, a, a, a, a, a, a, a, a, a
+#define mag_48(a) a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a, a
+
+class NewTurret;
+
+class CfgVehicles {
+	class LandVehicle;
+	class StaticWeapon: LandVehicle {
+        class ACE_SelfActions;
+        class Turrets {
+            class MainTurret: NewTurret {};
+		};
+	};
+	class StaticMGWeapon: StaticWeapon {
+        class ACE_SelfActions: ACE_SelfActions {};	
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {};
+        };		
+	};
+	class B_Ship_Gun_01_base_F: StaticMGWeapon {
+        class ACE_SelfActions: ACE_SelfActions {};
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {};
+		};
+	};	
+	class itc_land_NGS01_base: B_Ship_Gun_01_base_F {
+		class ACE_SelfActions: ACE_SelfActions {
+            class ITC_Land_SPHammohandler {
+                displayName = "Open Ammo Handling Interface";
+                condition = "( _target ) == ([] call itc_land_common_fnc_getCurVehicle)";
+                statement = "createDialog 'ITC_Land_SPHammohandler'";
+            };	
+		};	
+		artilleryScanner = 0;
+        class Turrets: Turrets {
+            class MainTurret: MainTurret {
+				turretInfoType = "ITC_Land_RscIGS_SPH";
+				weapons[] = {"itc_land_127mm_navalgunsystem"};
+				magazines[] = {
+					mag_48("itc_land_g127hex"),
+					mag_24("itc_land_g127smo"),
+					mag_24("itc_land_g127ill"),
+					mag_12("itc_land_g127pgm"),
+					mag_12("itc_land_g127lgm")
+				};
+				class OpticsIn: ITC_Land_Optics_IGS {
+					class Wide: Wide {};
+				};				
+			};
+		};	
+	};
+	class itc_land_NGS01_hammer2: itc_land_NGS01_base {
+		scope = 2;
+		author = "Toadball";
+		displayName = "Mk45 mod. 0 Hammer 2";
+		scopeCurator = 2;
+		side = 1;
+		faction = "BLU_F";
+		crew = "B_UAV_AI";
+		typicalCargo[] = {"B_UAV_AI"};		
+	};	
+};
---
+++ Addons/itc_land_veh_hammer/config.cpp
@@ -0,0 +1,23 @@
+ class CfgPatches {
+	class itc_land_veh_hammer	{
+		units[] = {	"itc_land_NGS01_hammer2"};
+		weapons[] = {};
+		requiredVersion = 1.8;
+		requiredAddons[] = {"A3_Weapons_F","itc_land_veh_sights","itc_land_veh_weapons","itc_land_sphammohandler"};
+		author = "ITC Addons Team";
+		authors[] = {"ToadBall","Yax"};
+	};
+};
+
+class RCWSOptics;
+class Optics_Armored {
+	class Wide: RCWSOptics {};
+};
+class Optics_Gunner_MBT_01: Optics_Armored {
+	class Wide: Wide {};
+};
+
+class ITC_Land_Optics_IGS: Optics_Gunner_MBT_01 {
+	class Wide: Wide {};
+};
+#include "CfgVehicles.hpp"
