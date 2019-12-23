c90320b7ecac7598c1d3f1ff539df6959643ca02
Andrew Somerville <asmsandrew@googlemail.com>
2019-12-23 22:22:27 +0000

    Added: Variants of strider labeled RV with new commanders optic

---
+++ Addons/itc_land_veh_strider/config.cpp
@@ -0,0 +1,9 @@
+class CfgPatches {
+	class itc_land_veh_strider {
+		requiredaddons[] = {"A3_Soft_F_Beta_MRAP_03","itc_land_veh_sights"};
+		requiredversion = 1.9;
+		units[] = {"itc_land_I_StriderRV","itc_land_I_StriderRV_HMG","itc_land_I_StriderRV_GMG"};
+	};
+};
+class NewTurret;
+#include "CfgVehicles.hpp"
