864f35fafd7acba752888460b8ddcb6ee542d3b0
Andrew Somerville <asmsandrew@googlemail.com>
2019-12-23 22:23:30 +0000

    Added: Variant of the Sentinel UCAV with improved drone sight

---
+++ Addons/itc_land_veh_uav05/config.cpp
@@ -0,0 +1,13 @@
+class CfgPatches {
+	class itc_land_veh_uav05 {
+		requiredaddons[] = {"A3_Air_F_Jets_UAV_05","itc_land_veh_sights"};
+		requiredversion = 1.9;
+		units[] = {"ITC_Land_B_UAV_UCAVi"};
+	};
+};
+
+class NewTurret;
+
+//#include "RscInGameUI.hpp"
+//#include "CfgFunctions.hpp"
+#include "CfgVehicles.hpp"
