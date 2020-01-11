4c9ac0d813353a12fb61109e1d57905e53d42742
Andrew Somerville <asmsandrew@googlemail.com>
2020-01-11 17:48:28 +0000

    Project: Spicy Rocket

---
+++ Addons/itc_land_spike/config.cpp
@@ -0,0 +1,11 @@
+class CfgPatches {
+	class itc_land_spike {
+		author = "ITC Addons Team";
+		authors[] = {"ToadBall","Yax","VKing"};
+		requiredaddons[] = {"A3_Weapons_F";
+		requiredversion = 1.9;
+		units[] = {};
+		weapons[] = {};
+		magazines[] = {};
+	};
+};
