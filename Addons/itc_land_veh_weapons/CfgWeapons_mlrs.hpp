cf858a51efce77d27bcd2ada6f4c27d185199e05
Andrew <asmsandrew@googlemail.com>
2021-04-05 23:24:11 +0100

    MLRS Btab revisions

--- Addons/itc_land_veh_weapons/CfgWeapons_mlrs.hpp
+++ Addons/itc_land_veh_weapons/CfgWeapons_mlrs.hpp
@@ -1,24 +1,41 @@
 class RocketPods;
 class rockets_230mm_GAT : RocketPods {
+  /*
+  class Mode_1;
+  class Mode_2;
+  class Mode_3;
+  class Mode_4;
+  class Mode_5;
+  class Mode_6;
+  class Mode_7;
+  class Mode_8;
+  class Mode_9;
+  class Mode_10;
+  class Mode_11;
+  class Mode_12;
+  class Mode_13;
+  class Mode_14;
+  class Mode_15;
   class Mode_16;
+  */
 };
 class itc_land_230mm_mlrs : rockets_230mm_GAT {
-  modes[] = {"Mode_16"};
-  displayName = "M230 MLRS";  
+  //modes[] = {"Mode_16"};
+  displayName = "M230 MLRS";
   magazineReloadTime = 30;
   magazines[] = {
     "itc_land_m230hex_12rnd",
     "itc_land_m230pgm_12rnd","itc_land_m230pgm_6rnd",
     "itc_land_m230icm_12rnd","itc_land_m230icm_6rnd",
-    "itc_land_m230gicm_12rnd","itc_land_m230gicm_6rnd",		
-	"itc_land_m230mat_12rnd","itc_land_m230map_12rnd"
+    "itc_land_m230gicm_12rnd","itc_land_m230gicm_6rnd",
+	  "itc_land_m230mat_12rnd","itc_land_m230map_12rnd"
   };
-  class Mode_16 : Mode_16 {
+  /*class Mode_16 : Mode_16 {
     displayName = "MLRS";
     artilleryCharge = 1;
 	   reloadTime = 0.5;
-	   
-  };
+
+  };*/
   class EventHandlers
   {
       class itc_land_veh_weapons
--- Addons/itc_land_veh_weapons/functions/adjustMuzzleVelG15x.sqf
+++ Addons/itc_land_veh_weapons/functions/adjustMuzzleVelG15x.sqf
@@ -1,7 +1,7 @@
 params ["_vehicle","_weapon","_muzzle","_mode","_ammo","_magazine","_projectile"];
 
 private _currentSpeed = vectorMagnitude (velocity _projectile);
-private _randomFactor = random [(_currentSpeed / 100) * -0.5,0,(_currentSpeed / 100) * 0.5];
+private _randomFactor = random [(_currentSpeed / 100) * -0.25,0,(_currentSpeed / 100) * 0.25];
 private _targetSpeed = _currentSpeed + _randomFactor;
 private _targetVelocity = (vectorNormalized velocity _projectile) vectorMultiply _targetSpeed;
 _projectile setVelocity _targetVelocity;
